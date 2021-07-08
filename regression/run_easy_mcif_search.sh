f7cae519bed128173499274f1d4afe83cca68041
Martin Steinegger <themartinsteinegger@gmail.com>
2021-07-08 17:36:02 +0900

    Add GZ test

--- regression/run_easy_mcif_search.sh
+++ regression/run_easy_mcif_search.sh
@@ -4,9 +4,12 @@ TARGET="${DATADIR}/scop"
 SCOPANOTATION="${DATADIR}/scop_lookup_bench.tsv"
 
 "${FOLDSEEK}" easy-search "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10000 -s 5 --max-seqs 100
+"${FOLDSEEK}" easy-search "${QUERY}.gz" "$TARGET" "$RESULTS/results_aln_gz.m8" "$RESULTS/tmp" -e 10000 -s 5 --max-seqs 100
 
-ACTUAL=$(wc -l "$RESULTS/results_aln.m8"|awk '{print $1 }')
-TARGET="14"
+ACTUAL1=$(wc -l "$RESULTS/results_aln.m8"|awk '{print $1 }')
+ACTUAL2=$(wc -l "$RESULTS/results_aln_gz.m8"|awk '{print $1 }')
+ACTUAL="${ACTUAL1} ${ACTUAL2}"
+TARGET="14 14"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
