f9e693ce50773e702ea2e6b9b935292137e89466
Martin Steinegger <themartinsteinegger@gmail.com>
2021-12-30 23:03:47 +0900

    Update regression test

--- regression/run_easy_search.sh
+++ regression/run_easy_search.sh
@@ -9,7 +9,7 @@ awk '{gsub("_[A-Za-z0-9]*$","",$1); gsub("_[A-Za-z0-9]*$","",$2); print $1"\t"$2
 "${EVALUATE}" "$SCOPANOTATION" "$RESULTS/results_aln.nochain.m8" > "${RESULTS}/evaluation.log"
 
 ACTUAL=$(awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' "${RESULTS}/evaluation.log")
-TARGET="0.886667 0.476667 0.258624"
+TARGET="0.886667 0.49 0.265313"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual >= target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
--- regression/run_search.sh
+++ regression/run_search.sh
@@ -8,6 +8,7 @@ SCOPANOTATION="${DATADIR}/scop_lookup_bench.tsv"
 TARGETDB="${RESULTS}/target"
 "${FOLDSEEK}" createdb "${TARGET}" "${TARGETDB}"
 
+"${FOLDSEEK}" createindex "$TARGETDB"  "$RESULTS/tmp" 
 "${FOLDSEEK}" search "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/tmp" -e 10000 -s 5 --max-seqs 100
 "${FOLDSEEK}" convertalis "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/results_aln.m8"
 
@@ -15,7 +16,7 @@ awk '{gsub("_[A-Za-z0-9]*$","",$1); gsub("_[A-Za-z0-9]*$","",$2); print $1"\t"$2
 "${EVALUATE}" "$SCOPANOTATION" "$RESULTS/results_aln.nochain.m8" > "${RESULTS}/evaluation.log"
 
 ACTUAL=$(awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' "${RESULTS}/evaluation.log")
-TARGET="0.886667 0.476667 0.258624"
+TARGET="0.886667 0.49 0.265313"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual >= target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
