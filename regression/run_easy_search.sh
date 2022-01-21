f4bbc830b124b99c07ba1e17629546fb0358ce75
Martin Steinegger <themartinsteinegger@gmail.com>
2022-01-21 22:09:21 +0900

    Update run_easy_search.sh

--- regression/run_easy_search.sh
+++ regression/run_easy_search.sh
@@ -3,12 +3,12 @@ QUERY="${DATADIR}/scop"
 TARGET="${DATADIR}/scop"
 SCOPANOTATION="${DATADIR}/scop_lookup_bench.tsv"
 
-"${FOLDSEEK}" easy-search "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10000 -s 5 --max-seqs 100
+"${FOLDSEEK}" easy-search "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10000 -s 9 --max-seqs 100
 
 "${EVALUATE}" "$SCOPANOTATION" "$RESULTS/results_aln.m8" > "${RESULTS}/evaluation.log"
 
 ACTUAL=$(awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' "${RESULTS}/evaluation.log")
-TARGET="0.886667 0.49 0.265313"
+TARGET="0.986667 0.77101 0.435409"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual >= target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
