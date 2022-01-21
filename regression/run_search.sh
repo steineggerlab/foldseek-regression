8717f2f38a494913346a4cda510a7e558b5d22a8
Martin Steinegger <themartinsteinegger@gmail.com>
2022-01-21 22:05:06 +0900

    Update run_search.sh

--- regression/run_search.sh
+++ regression/run_search.sh
@@ -9,13 +9,13 @@ TARGETDB="${RESULTS}/target"
 "${FOLDSEEK}" createdb "${TARGET}" "${TARGETDB}" 
 
 "${FOLDSEEK}" createindex "$TARGETDB"  "$RESULTS/tmp" 
-"${FOLDSEEK}" search "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/tmp" -e 10000 -s 5 --max-seqs 100
+"${FOLDSEEK}" search "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/tmp" -e 10000 -s 9 --max-seqs 100
 "${FOLDSEEK}" convertalis "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/results_aln.m8"
 
 "${EVALUATE}" "$SCOPANOTATION" "$RESULTS/results_aln.m8" > "${RESULTS}/evaluation.log"
 
 ACTUAL=$(awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' "${RESULTS}/evaluation.log")
-TARGET="0.886667 0.49 0.265313"
+TARGET="0.986667 0.77101 0.435409"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual >= target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
