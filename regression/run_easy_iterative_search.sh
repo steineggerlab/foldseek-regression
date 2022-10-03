be99484f50f4400bd36d6e877523f763af175732
Martin Steinegger <themartinsteinegger@gmail.com>
2022-10-03 23:12:03 +0900

    Add iterative benchmark

---
+++ regression/run_easy_iterative_search.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -ex
+QUERY="${DATADIR}/scop"
+TARGET="${DATADIR}/scop"
+SCOPANOTATION="${DATADIR}/scop_lookup_bench.tsv"
+
+"${FOLDSEEK}" easy-search "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10000 -s 9 --max-seqs 100 --num-iterations 3 --remove-tmp-files 0
+
+"${EVALUATE}" "$SCOPANOTATION" "$RESULTS/results_aln.m8" > "${RESULTS}/evaluation.log"
+
+ACTUAL=$(awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' "${RESULTS}/evaluation.log")
+TARGET="0.986667 0.836061 0.45569"
+awk -v actual="$ACTUAL" -v target="$TARGET" \
+    'BEGIN { print (actual >= target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
+    > "${RESULTS}.report"
