01d685b049924f580eb9fbf59542427a7d05fcc1
Martin Steinegger <themartinsteinegger@gmail.com>
2022-12-20 18:54:26 +0900

    Adjust to new tmalign search workflow

--- regression/run_search_tm.sh
+++ regression/run_search_tm.sh
@@ -14,7 +14,7 @@ TARGETDB="${RESULTS}/target"
 "${EVALUATE}" "$SCOPANOTATION" "$RESULTS/results_aln.m8" > "${RESULTS}/evaluation.log"
 
 ACTUAL=$(awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' "${RESULTS}/evaluation.log")
-TARGET="0.873333 0.463333 0.287054"
+TARGET="0.873333 0.463333 0.274871"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual >= target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
