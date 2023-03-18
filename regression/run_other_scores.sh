fe7ffb6cde897a9bdbd89bce5f9c34bd837fd376
Martin Steinegger <themartinsteinegger@gmail.com>
2023-03-18 18:19:09 +0900

    Add qtmscore and ttmscore

--- regression/run_other_scores.sh
+++ regression/run_other_scores.sh
@@ -10,8 +10,7 @@ TARGETDB="${RESULTS}/target"
 
 "${FOLDSEEK}" createindex "$TARGETDB"  "$RESULTS/tmp"
 "${FOLDSEEK}" search "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/tmp" -e 10000 -s 3 --max-seqs 100 -a
-"${FOLDSEEK}" convertalis "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/results_aln.m8" --format-output "query,target,lddt,prob,rmsd,alntmscore,u,t,lddtfull"
-
+"${FOLDSEEK}" convertalis "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/results_aln.m8" --format-output "query,target,lddt,prob,rmsd,alntmscore,qtmscore,ttmscore,u,t,lddtfull"
 head -n 3  "$RESULTS/results_aln.m8" | tail -n 1 \
-  | awk '{ print ($3 == 3.475E-01 && $4 == 1.000 && $5 == 1.074E+01 && $6 == 4.630E-01) ? "GOOD" : "BAD"; }' \
+  | awk '{ print ($3 == 3.475E-01 && $4 == 1.000 && $5 == 1.074E+01 && $6 == 4.630E-01 && $7 == 4.081E-01 && $8 == 4.630E-01) ? "GOOD" : "BAD"; }' \
   > "${RESULTS}.report"
