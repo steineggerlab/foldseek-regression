7e1b892de661db251891d08ec98b63d523c745f4
Martin Steinegger <themartinsteinegger@gmail.com>
2023-05-05 22:21:20 +0900

    Update run_easy_caonly_search.sh

--- regression/run_easy_caonly_search.sh
+++ regression/run_easy_caonly_search.sh
@@ -12,10 +12,10 @@ set -x
 QUERY="$RESULTS/scop_caonly/d1alva_"
 TARGET="$RESULTS/scop_caonly"
 
-"${FOLDSEEK}" easy-search "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10000 -s 9 --max-seqs 100
+"${FOLDSEEK}" easy-search "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10 -s 9 --max-seqs 100
 
 ACTUAL=$(wc -l "$RESULTS/results_aln.m8"|awk '{print $1 }')
-TARGET="72"
+TARGET="27"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
