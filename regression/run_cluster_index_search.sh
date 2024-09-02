57dcb544ddfed50aee73f6c4070dc0c3dcf37a9d
SooyoungCha <ellen2g77@gmail.com>
2024-09-03 00:06:15 +0900

    alntm run_cluster_index_search.sh

--- regression/run_cluster_index_search.sh
+++ regression/run_cluster_index_search.sh
@@ -13,7 +13,7 @@ SCOPANOTATION="${DATADIR}/scop_lookup_bench.tsv"
 "${EVALUATE}" "$SCOPANOTATION" "$RESULTS/results_aln.m8" > "${RESULTS}/evaluation.log"
 
 ACTUAL=$(awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' "${RESULTS}/evaluation.log")
-TARGET="0.986667 0.806061 0.449434"
+TARGET="0.986667 0.786061 0.445521"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual >= target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
