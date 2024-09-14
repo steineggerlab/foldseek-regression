8aa9946484a2177af040ecc8b1e550c434114e37
Martin Steinegger <themartinsteinegger@gmail.com>
2024-09-14 23:07:49 +0200

    Update run_other_scores.sh

--- regression/run_other_scores.sh
+++ regression/run_other_scores.sh
@@ -12,5 +12,5 @@ TARGETDB="${RESULTS}/target"
 "${FOLDSEEK}" search "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/tmp" -e 10000 -s 3 --max-seqs 100 -a
 "${FOLDSEEK}" convertalis "$QUERYDB" "$TARGETDB" "$RESULTS/results_aln" "$RESULTS/results_aln.m8" --format-output "query,target,lddt,prob,rmsd,alntmscore,qtmscore,ttmscore,u,t,lddtfull"
 head -n 3  "$RESULTS/results_aln.m8" | tail -n 1 \
-  | awk '{ print ($3 == 3.483E-01 && $4 == 1.000 && $5 == 1.068E+01 && $6 == 4.029E-01 && $7 == 4.062E-01 && $8 == 4.613E-01) ? "GOOD" : "BAD"; }' \
+  | awk '{ print ($3 == 3.483E-01 && $4 == 1.000 && $5 == 1.068E+01 && $6 == 4.703E-01 && $7 == 4.062E-01 && $8 == 4.613E-01) ? "GOOD" : "BAD"; }' \
   > "${RESULTS}.report"
