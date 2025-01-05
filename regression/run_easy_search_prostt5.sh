166548328ebe2aa3dc3b6533034d2d9ff07657b0
Milot Mirdita <milot@mirdita.de>
2025-01-05 19:57:02 +0900

    Print ggml debug output in prostt5 regression

--- regression/run_easy_search_prostt5.sh
+++ regression/run_easy_search_prostt5.sh
@@ -7,8 +7,10 @@ cat <<EOF > "${QUERY}"
 SATDAFWAKYTACESARNPREKLNECLEGN
 EOF
 
-"${FOLDSEEK}" databases ProstT5 "$RESULTS/pt5" "$RESULTS/tmp"
-"${FOLDSEEK}" easy-search "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10000 -s 9 --max-seqs 100 --prostt5-model "$RESULTS/pt5"
+if [ ! -e "$RESULTS/pt5/prostt5-f16.gguf" ]; then
+    "${FOLDSEEK}" databases ProstT5 "$RESULTS/pt5" "$RESULTS/tmp"
+fi
+"${FOLDSEEK}" easy-search "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10000 -s 9 --max-seqs 100 --prostt5-model "$RESULTS/pt5" -v 4 --threads 1
 
 awk -v target="d2pf1a2" \
     'NR == 1 { result = "BAD"; if ($1 == $2 && $1 == target) result = "GOOD"; printf("%s\nExpected:\n%s %s\nActual:\n%s %s\n", result, target, target, $1, $2); exit; }' \
