464a5fdd9ea1ba7ee0d4fadc92f2d49dba526b5c
Sooyoung Cha <ellen2g@snu.ac.kr>
2025-12-24 16:46:42 +0900

    change in parameter name

--- regression/run_clusearch_multimersearch.sh
+++ regression/run_clusearch_multimersearch.sh
@@ -5,7 +5,7 @@ QUERY="${DATADIR}/complex/complexdir.tar.gz"
 "${FOLDSEEK}" createdb "$QUERY" "$RESULTS/db"
 "${FOLDSEEK}" linclust "$RESULTS/db" "$RESULTS/clu" "$RESULTS/clutmp" --min-seq-id 0.3
 "${FOLDSEEK}" createclusearchdb "$RESULTS/db" "$RESULTS/clu" "$RESULTS/clusearchdb"
-"${FOLDSEEK}" easy-multimersearch "$RESULTS/db" "$RESULTS/clusearchdb" "$RESULTS/aln" "$RESULTS/tmp" --chain-tm-threshold 0.5 --tmscore-threshold 0 --interface-lddt-threshold 0.3 --cov-mode 1
+"${FOLDSEEK}" easy-multimersearch "$RESULTS/db" "$RESULTS/clusearchdb" "$RESULTS/aln" "$RESULTS/tmp" --chain-tm-threshold 0.5 --multimer-tm-threshold 0 --interface-lddt-threshold 0.3 --cov-mode 1
 
 ACTUAL=$(awk 'BEGIN{qtm=0;ttm=0;n=0}{qtm+=$5;ttm+=$6;n++}END{print qtm,ttm,n}' "$RESULTS/aln_report")
 
