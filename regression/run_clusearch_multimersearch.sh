eb22f133de2b4a80304a0eea93515ada446a44c8
SooyoungCha <ellen2g77@gmail.com>
2026-03-10 18:11:25 +0900

    one nofilter, one filter

--- regression/run_clusearch_multimersearch.sh
+++ regression/run_clusearch_multimersearch.sh
@@ -5,11 +5,11 @@ QUERY="${DATADIR}/complex/complexdir.tar.gz"
 "${FOLDSEEK}" createdb "$QUERY" "$RESULTS/db"
 "${FOLDSEEK}" linclust "$RESULTS/db" "$RESULTS/clu" "$RESULTS/clutmp" --min-seq-id 0.3
 "${FOLDSEEK}" createclusearchdb "$RESULTS/db" "$RESULTS/clu" "$RESULTS/clusearchdb"
-"${FOLDSEEK}" easy-multimersearch "$RESULTS/db" "$RESULTS/clusearchdb" "$RESULTS/aln" "$RESULTS/tmp" --chain-tm-threshold 0.5 --multimer-tm-threshold 0 --interface-lddt-threshold 0.3 --cov-mode 1
+"${FOLDSEEK}" easy-multimersearch "$RESULTS/db" "$RESULTS/clusearchdb" "$RESULTS/aln" "$RESULTS/tmp" --chain-tm-threshold 0.5 --multimer-tm-threshold 0 --interface-lddt-threshold 0.3 --cov-mode 1 --no-filter 0
 
 ACTUAL=$(awk 'BEGIN{qtm=0;ttm=0;n=0}{qtm+=$5;ttm+=$6;n++}END{print qtm,ttm,n}' "$RESULTS/aln_report")
 
-TARGET="240.751 1110.31 10967"
+TARGET="77.7697 176.755 256"
 
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
