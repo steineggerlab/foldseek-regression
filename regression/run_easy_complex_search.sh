84208d41578c157c4365612cd8b19ff41289407b
SooyoungCha <ellen2g77@gmail.com>
2026-03-16 16:15:21 +0900

    no newline for macOS

--- regression/run_easy_complex_search.sh
+++ regression/run_easy_complex_search.sh
@@ -4,9 +4,9 @@ TARGET="${DATADIR}/complex/8tim.cif"
 
 "${FOLDSEEK}" easy-complexsearch "$QUERY.gz" "$TARGET.gz" "$RESULTS/easyCompAln" "$RESULTS/tmp" 
 
-ACTUAL=$(awk '{print $3,$4,$5,$6}' "$RESULTS/easyCompAln_report")
+ACTUAL=$(awk 'FNR==2{print $3,$4,$5,$6}' "$RESULTS/easyCompAln_report")
 
-TARGET="A,B A,B 0.98941 0.98941\nA,B B,A 0.98703 0.98703"
+TARGET="A,B B,A 0.98703 0.98703"
 
 
 awk -v actual="$ACTUAL" -v target="$TARGET" \
